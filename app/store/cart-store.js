bbae96ae464d7657a3392a1f228ba0d7f665d77f
Sujhan-64 <sujhan.siva@gmail.com>
2025-11-12 15:22:07 +0530

    resolved duplicate item in cart

--- app/store/cart-store.js
+++ app/store/cart-store.js
@@ -2,9 +2,14 @@ import { create } from "zustand";
 import {persist, createJSONStorage} from "zustand/middleware"
 import {shallow} from "zustand/shallow"
 export const useCartStore = create(
-    persist((set) => ({
+    persist((set,get) => ({
     cart : [],
-    addToCart : (product) => set((state) => ({cart: [...state.cart, product]})),
+    addToCart : (product) =>{
+       const exsists = get().cart.some((item) => item._id === product._id)
+       if(!exsists){
+        set((state) => ({cart: [...state.cart, product]}))
+       }
+    },
     removeFromCart: (productId) => set((state) => ({cart : state.cart.filter(product =>product._id!==productId)})),
     clearCart : () => set({cart : []}),
     }),
@@ -20,6 +25,7 @@ export const useCartStore = create(
 //The outer arrow function is used because we are creating a custom react hook. so its a function returning a function
 export const useCart = () => useCartStore((state) => state.cart)
 
+//custon cart action hooks
 export const useCartActions = () => {
   const addToCart = useCartStore((state) => state.addToCart);
   const removeFromCart = useCartStore((state) => state.removeFromCart);
